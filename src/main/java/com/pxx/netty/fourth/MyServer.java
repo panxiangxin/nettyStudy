f3db8ab928f9ce534aa1b81817f41586908d91c7
maybe <www.721297578@qq.com>
2019-11-13 16:17:24 +0800

    commit

--- src/main/java/com/pxx/netty/fourth/MyServer.java
+++ src/main/java/com/pxx/netty/fourth/MyServer.java
@@ -1,9 +1,34 @@
 package com.pxx.netty.fourth;
 
+import com.pxx.netty.third.MyChatServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
 /**
  * @author pxx
  * Date 2019/11/13 9:17
  * @Description
  */
 public class MyServer {
+	public static void main(String[] args) throws Exception {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		
+		try {
+			ServerBootstrap serverBootstrap = new ServerBootstrap();
+			serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+					.handler(new LoggingHandler(LogLevel.INFO))
+					.childHandler(new MyServerInitializer());
+			ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+			channelFuture.channel().closeFuture().sync();
+		}finally {
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
 }
--- src/main/java/com/pxx/netty/third/MyChatClient.java
+++ src/main/java/com/pxx/netty/third/MyChatClient.java
@@ -1,15 +1,12 @@
 package com.pxx.netty.third;
 
-import com.pxx.netty.second.MyClientInitializer;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
